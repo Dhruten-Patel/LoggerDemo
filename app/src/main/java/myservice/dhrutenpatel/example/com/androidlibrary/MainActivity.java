2636081914adf69177652b7b61edc7aa2c555bae
dhruten <dhrutenpatel@gmail.com>
2019-06-19 16:58:12 +0530

    Create project Logger

---
+++ app/src/main/java/myservice/dhrutenpatel/example/com/androidlibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package myservice.dhrutenpatel.example.com.androidlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ logutil/src/main/java/myservice/dhrutenpatel/example/com/logutil/LogDebug.java
@@ -0,0 +1,9 @@
+package myservice.dhrutenpatel.example.com.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+    public static void d(String msg){
+        Log.d("Dhruten_Patel","Hello This is my First Library\n -Dhruten Patel");
+    }
+}
